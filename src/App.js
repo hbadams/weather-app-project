60d97385c703ad0483c504db0428f6c91cffbe95
hbadams <channell350@gmail.com>
2022-09-06 15:18:39 -0400

    Added GitHub repository to page footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Hayley Adams, and is{" "}
+        <a
+          href="https://github.com/hbadams/weather-app-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

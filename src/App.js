e583e60f65409f46570147b899ee7d512a2127fb
hbadams <channell350@gmail.com>
2022-09-06 16:04:21 -0400

    Added new Weather component, built app skeleton ready for css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Hayley Adams, and is{" "}
-        <a
-          href="https://github.com/hbadams/weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Hayley Adams, and is{" "}
+          <a
+            href="https://github.com/hbadams/weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
